3278a245db4a444b1ffb49a12b8edc957d0fa8b1
kkumcenter <kkumcenter@users.noreply.github.com>
2026-05-28 08:34:10 +0900

    Connect Supabase project config

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 window.KKOOM_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://aaxkswurqysmthxmdyst.supabase.co",
+  anonKey: "sb_publishable_xiDTLnurGItJV7JrZrCTlg_UZGbPrIq",
   redirectTo: "https://kkumcenter.github.io/kkumcenter/login.html"
 };
